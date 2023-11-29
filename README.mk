4776951f829c35742744af50f85507ff0824ca0e
Balofoide <Balofoide@gmail.com>
2023-11-29 16:40:13 -0300

    Readme

--- README.mk
+++ README.mk
@@ -1,2 +1,27 @@
-# Estudos de C++
-Este repositório contém os meus arquivos e projetos relacionados aos meus estudos de C++.
+Estudos de Programação
+Este repositório é um arquivo de anotações dos meus estudos em diversas linguagens de programação.
+
+Sobre
+Aqui estão minhas anotações, códigos e projetos relacionados aos meus estudos em diferentes linguagens de programação. Cada diretório é dedicado a uma linguagem específica.
+
+Estrutura
+
+/C++: Códigos e projetos relacionados à linguagem C++.
+/Lua: Codigos e projetos relacionados à linguagem Lua.
+/Asembly: Códigos e projetos relacionados à linguagem Asembly.
+
+
+Como Contribuir
+Se encontrar erros, tiver sugestões ou quiser contribuir com mais informações para qualquer linguagem, sinta-se à vontade para abrir issues ou enviar pull requests.
+
+
+Recursos
+Além das anotações, este repositório também pode conter referências a livros, tutoriais online e sites úteis para cada linguagem específica.
+
+
+Nota
+Este repositório serve como um repositório central para meus estudos de programação. As anotações aqui podem variar em profundidade e complexidade, refletindo meu processo de aprendizado.
+
+Sinta-se à vontade para explorar e usar as informações aqui para o seu próprio aprendizado!
+
+
